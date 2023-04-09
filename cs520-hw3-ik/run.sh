18ce07137ebdeb23c5c0ccba25820564251b067f
tutor-1v1 <liumeng64@gmail.com>
2023-04-09 18:39:37 +0800

    WeChat cstutorcs CS520 Assignment 3 Inverse Kinematics with Skinning

---
+++ cs520-hw3-ik/run.sh
@@ -0,0 +1,12 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+cd armadillo
+#cd dragon
+#cd hand
+../driver skin.config
